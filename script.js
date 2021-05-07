f807c234cd82d2ad4cf5b171bbcb3fa6644d67c2
Ashwin Dhuriya <52548434+Ashwin1729@users.noreply.github.com>
2021-05-07 17:04:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+  let guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too High!';
+    if (score > 0) {
+      score--;
+    }
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too Low!';
+    if (score > 0) {
+      score--;
+    }
+  }
+  if (score === 0) {
+    document.querySelector('.message').textContent = '💥 You Loose!';
+  }
+  document.querySelector('.score').textContent = score;
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('body').style.backgroundColor = '#222';
+});
